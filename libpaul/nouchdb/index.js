b03d20732f6139c13c3145aa8ca57616ace1bd31
Anabelle <git@anabelle.dev>
2023-10-07 16:24:15 -0400

    change port, explode json

--- libpaul/nouchdb/index.js
+++ libpaul/nouchdb/index.js
@@ -2,6 +2,16 @@ var PouchDB = require('pouchdb');
 var express = require('express');
 var app = express();
 
-app.use('/', require('express-pouchdb')(PouchDB));
+app.use(
+    '/', 
+    require('express-pouchdb') (
+        PouchDB, {
+            auth: {
+                username: 'john',
+                password: 'smith'
+            }
+        }
+    )
+);
 
-app.listen(3000);
+app.listen(5984);
