ca9892a152c54d4132f30fd888386a3572731d77
Anabelle <git@anabelle.dev>
2023-10-07 20:11:40 -0400

    change libpaul subprocess

--- libpaul/main.go
+++ libpaul/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"time"
+
+	_ "github.com/go-kivik/couchdb/v3"
+
+	"github.com/go-kivik/kivik"
+	"github.com/go-kivik/kivik/v3"
+)
+
+// The CouchDB driver
+
+
 
 func main() {
-	fmt.Println("SUBPROCESS DESU")
+	fmt.Println("SUBPROCESS です")
+
+	//run couch
+	exec.Command("node", "couch.js")
+
+	//Don't @ me, there's probably some way to wait properly
+	time.Sleep(2 * time.Second)
+
+	dsn := "http://localhost:5984"
+	client, err := kivik.New(context.TODO(), "couch", dsn)
+
+	if err != nil {
+		panic(err)
+	}
+	err.
 }
